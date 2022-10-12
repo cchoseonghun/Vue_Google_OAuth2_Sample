14e0c97ed0a4c3d28181b1d301c9daa75961bdd7
seonghun <cchoseonghun@gmail.com>
2022-10-12 18:19:13 +0900

    first upload

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id='G_OAuth_btn'></div>
+    <button v-if="Object.keys(user).length != 0" @click="handleSignOut">Sign Out</button>
+    <div v-if="user">
+        <img :src="user.picture" />
+        <h3>{{user.name}}</h3>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueJwtDecode from 'vue-jwt-decode'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      user: {}, 
+    }
+  }, 
+  mounted(){
+    let google = window.google;
+    google.accounts.id.initialize({
+      client_id: '클라이언트 ID', 
+      callback: this.handleCallbackResponse
+    })
+    google.accounts.id.renderButton(
+      document.getElementById('G_OAuth_btn'), 
+      { theme: 'outline', size: 'large' }
+    )
+  }, 
+  methods: {
+    handleCallbackResponse(res){
+      console.log('Encoded JWT ID token: ' + res.credential);
+
+      let userObject = VueJwtDecode.decode(res.credential);
+      console.log(userObject);
+      this.user = userObject;
+      document.querySelector('#G_OAuth_btn').hidden = true;
+
+    }, 
+    handleSignOut(){
+      this.user = {};
+      document.querySelector('#G_OAuth_btn').hidden = false;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
